6c2952713af776519e50da9c3e7b1f305bdf49ff
wangenius <wangenius@qq.com>
2024-02-28 21:47:03 +0800

    修复了地址bug

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,79 +1,87 @@
 import { useEffect, useState } from "react";
-import style from "./index.module.scss"
+import style from "./index.module.scss";
 
 export const Tag = ({ children }) => {
   return <span className={style.tag}>{children}</span>;
 };
 
-
-export const Tags = ({items}) =>{
-
-  return <div className={style.tags}>
-    {items.map((item,key)=>{
-      return <Tag key={key}>{item}</Tag>
-    })}
-  </div>
-}
-
-
-function checkImgExists(imgurl: string) {
-  return new Promise(function(resolve, reject) {
-    var ImgObj = new Image();
-    ImgObj.src = imgurl;
-    ImgObj.onload = function(res) {      
-      resolve(res);
-    }
-    ImgObj.onerror = function(err) {
-      reject(err)
-    }
-  })
-}
-
-
-
-const webicon = "https://www.svgrepo.com/show/331784/web-sites.svg"
-/** @description 外部链接 */
-export const To = ({name,icon,to}:{name:string,icon?:string,to:string})=>{
-
-  const [url,setUrl] = useState(webicon)
-  useEffect(()=>{
-    if(icon) setUrl(icon);
-    if(!icon) checkImgExists(to + "/favicon.ico")
-    .then(()=>{setUrl(to+"/favicon.ico")})
-  .catch(( )=>{})
-  },[])
-  
-  return <a target="_blank" className={style.to} href={to}>
-    <div className={style.top}>
-    <span>
-      <img src={url} />
-    </span>
-    <span style={{flex:1}}>{name}</span>
+export const Tags = ({ items }) => {
+  return (
+    <div className={style.tags}>
+      {items.map((item, key) => {
+        return <Tag key={key}>{item}</Tag>;
+      })}
     </div>
-   
-    <span className={style.bottom}>{to}</span>
-  </a>
+  );
+};
+
+function checkImgExists(img_url: string) {
+  return new Promise(function (resolve, reject) {
+    var ImgObj = new Image();
+    ImgObj.src = img_url;
+    ImgObj.onload = function (res) {
+      resolve(res);
+    };
+    ImgObj.onerror = function (err) {
+      reject(err);
+    };
+  });
 }
 
-export const Multi = ({children}) =>{
-  return <div className={style.multi}>
-{children}
-  </div>
-}
+const web_icon = "https://www.svgrepo.com/show/331784/web-sites.svg";
+/** @description 外部链接 */
+export const To = ({
+  name,
+  icon,
+  to,
+}: {
+  name: string;
+  icon?: string;
+  to: string;
+}) => {
+  const [url, setUrl] = useState(web_icon);
+  useEffect(() => {
+    if (icon) setUrl(icon);
+    if (!icon)
+      checkImgExists(to + "/favicon.ico")
+        .then(() => {
+          setUrl(to + "/favicon.ico");
+        })
+        .catch(() => {});
+  }, []);
 
-export const Pairs = ({children,title}) => {
+  return (
+    <a target="_blank" className={style.to} href={to}>
+      <div className={style.top}>
+        <span>
+          <img src={url} />
+        </span>
+        <span style={{ flex: 1 }}>{name}</span>
+      </div>
 
-  return <div className={style.pairs}>
-    <div className={style.title}>{title}</div>
-    {children}
-  </div>
-}
+      <span className={style.bottom}>{to}</span>
+    </a>
+  );
+};
 
+export const Multi = ({ children }) => {
+  return <div className={style.multi}>{children}</div>;
+};
 
-export const Pair = ({children,title}) =>{
+export const Pairs = ({ children, title }) => {
+  return (
+    <div className={style.pairs}>
+      <div className={style.title}>{title}</div>
+      {children}
+    </div>
+  );
+};
 
-  return <div className={style.pair}>
-    <span>{title}</span>
-    <span>{children}</span>
-  </div>
-}
+export const Pair = ({ children, title }) => {
+  return (
+    <div className={style.pair}>
+      <span>{title}</span>
+      <span>{children}</span>
+    </div>
+  );
+};
